e5481c42a3faf98dfdf0da7d18090664cd134678
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2018-11-27 06:09:31 -0600

    renamed tests4

--- test/Pathing.Tests.FSharp/PathingValidationTests.fs
+++ test/Pathing.Tests.FSharp/PathingValidationTests.fs
@@ -4,21 +4,21 @@ open Xunit
 open Pathing
 open System
 
-module PathingTests = 
+module PathingValidationTests = 
   [<Fact>]
-  let ``Can construct AbsoluteDirectoryPath`` () =
+  let ``Can construct AbsoluteDirectoryPath from valid path`` () =
     Pathing.ToAbsoluteDirectoryPath "C:\\" |> ignore
 
   [<Fact>]
-  let ``Can construct RelativeDirectoryPath`` () =
+  let ``Can construct RelativeDirectoryPath from valid path`` () =
     Pathing.ToRelativeDirectoryPath "." |> ignore
 
   [<Fact>]
-  let ``Can construct AbsoluteFilePath`` () =
+  let ``Can construct AbsoluteFilePath from valid path`` () =
     Pathing.ToAbsoluteFilePath "C:\\x.txt" |> ignore
 
   [<Fact>]
-  let ``Can construct RelativeFilePath`` () =
+  let ``Can construct RelativeFilePath from valid path`` () =
     Pathing.ToRelativeFilePath "x.txt" |> ignore
   
 
@@ -44,22 +44,23 @@ module PathingTests =
       Pathing.ToRelativeFilePath null |> ignore) |> ignore
 
 
+
   [<Fact>]
   let ``Cannot construct AbsoluteDirectoryPath from empty string`` () =
-    Assert.Throws<ArgumentNullException> (fun () -> 
+    Assert.Throws<ArgumentException> (fun () -> 
       Pathing.ToAbsoluteDirectoryPath "" |> ignore) |> ignore
 
   [<Fact>]
   let ``Cannot construct RelativeDirectoryPath from empty string`` () =
-    Assert.Throws<ArgumentNullException> (fun () -> 
+    Assert.Throws<ArgumentException> (fun () -> 
       Pathing.ToRelativeDirectoryPath "" |> ignore) |> ignore
 
   [<Fact>]
   let ``Cannot construct AbsoluteFilePath from empty string`` () =
-    Assert.Throws<ArgumentNullException> (fun () -> 
+    Assert.Throws<ArgumentException> (fun () -> 
       Pathing.ToAbsoluteFilePath "" |> ignore) |> ignore
 
   [<Fact>]
   let ``Cannot construct RelativeFilePath from empty string`` () =
-    Assert.Throws<ArgumentNullException> (fun () -> 
+    Assert.Throws<ArgumentException> (fun () -> 
       Pathing.ToRelativeFilePath "" |> ignore) |> ignore
