f549e1b66b718736632cc768a428138f8e9a9baa
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2018-11-29 14:37:34 -0600

    ok

--- src/Pathing/Library.fs
+++ src/Pathing/Library.fs
@@ -2,20 +2,21 @@ namespace Pathing
 
 open System
 
-type PathSegment private (name) =
-  member this.Name = name
-  static member Create(name) = new PathSegment(name)
+type PathSegment = {
+  Name:string
+}
+with static member Create(name) = { Name = name }
 
 [<AbstractClass>]
 type AbstractPath (path) =    
   do match path with
-     | null -> nullArg "path" 
+     | null -> nullArg "path"
      | "" -> invalidArg "path" "'' is not a valid path."
      | _ -> ()
      
   member this.Segments = 
     path.Split('\\', '/') 
-    |> Seq.map PathSegment.Create
+    |> Seq.map (fun s -> { Name = s })
 
 type AbsoluteDirectoryPath private(path) =
   inherit AbstractPath(path)
--- test/Pathing.Tests.FSharp/PathParsingTests.fs
+++ test/Pathing.Tests.FSharp/PathParsingTests.fs
@@ -2,19 +2,17 @@
 
 open Xunit
 open Pathing
-open System
 
 module PathParsingTests =
-  open System.Collections.Generic
 
   [<Fact>]
-  let ``Relative directory path "x" has one segment`` () =
+  let ``Relative directory path "x" has expected segment`` () =
    let path = RelativeDirectoryPath.Create "x" in
    let segmentCount = path.Segments |> Seq.length in
    Assert.Equal(1, segmentCount)
     
   [<Fact>]
-  let ``Relative directory path ".\\x" has two segments`` () =
+  let ``Relative directory path ".\\x" has expected segments`` () =
     let expected = ["."; "x"] |> Seq.map PathSegment.Create in
     let path = RelativeDirectoryPath.Create ".\\x" in 
     let getName = fun (x:PathSegment) -> x.Name in
